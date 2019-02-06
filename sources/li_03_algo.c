4bc49b7c4712cdc1e25485af06f192431e1b6369
Jacques DOUNIOL <jdouniol@e1r12p10.42.fr>
2019-02-07 00:48:50 +0100

    ajout du poids

--- sources/li_03_algo.c
+++ sources/li_03_algo.c
@@ -13,6 +13,113 @@ void	ft_print_path(int *tab_of_path)
 }
 
 /*
+typedef	struct 		s_room // 
+{
+	int 			room_id;
+	int				number_ants_in_room; //nb_a
+	char 			*name_room; //name
+	int				coord_x_room; // x
+	int				coord_y_room; // y
+	int				*linked_to_start;
+	int				*linked_to_end;
+	int 			*names_path_available; //
+	int				number_path_availbale;
+	int				distance; // *distance ?
+	int				compteur_from_start;
+	int				compteur_from_end;
+	int				you_should_not_pass;
+	int				*linked_to;
+	int				number_rooms_linked;
+	struct s_room	*prev;
+	struct s_room	*next;
+	struct s_room	**tab;
+	t_status		status;
+	t_available		available;
+}
+*/
+
+void	ft_print_matrice_weight(t_room ***matrice, t_all *elem)
+{
+	int i;
+	int j;
+
+	i = 0;
+	print_all_name(*elem);
+	while (i < elem->number_rooms)
+	{
+		j = 0;
+		print_room(*elem, i);
+		while (j < elem->number_rooms)
+		{
+			if (matrice[i][j])
+				ft_printf("%-10d", matrice[i][j]->weight);
+			else
+				ft_printf("%-10d", 0);
+			j++;
+		}
+		ft_printf("\n");
+		i++;
+	}
+	ft_printf("\n\n\n");
+}
+
+int		ft_anybody_else_here(t_all *elen, t_room ***matrice, int current_room_id)
+{
+	int j;
+
+	j = 0;
+	while (j < elem->number_rooms)
+	{
+		if (matrice[current_room_id][j] != NULL && matrice[current_room_id][j]->weight < 0)
+			return (1);
+		else
+			j++;
+	}
+	if (j == elem->number_rooms)
+		return (0);
+
+}
+
+	int				*tmp_tab_room;
+	int				size_tmp_tab_room;
+
+int		ft_add_weight(t_all *elem, t_room ***matrice, int weight, int current_room_id)
+{
+	int i;
+	int j;
+	int k;
+	t_room *tmp;
+
+	i = current_room_id;
+	j = 0;
+	k = elem->end_id;
+	while (ft_anybody_else_here(elem, matrice, i))
+	{	
+		while (j < elem->number_rooms)
+		{
+			if (matrice[i][j] && matrice[i][j]->weight == -1)
+				{
+					elem->tmp_tab_room[elem->size_tmp_tab_room] = j;
+					elem->size_tmp_tab_room = elem->size_tmp_tab_room + 1;
+					matrice[i][j]->weight == weight;
+				}
+			j++
+		}
+		if (j == elem->number_rooms)
+		{
+			while (elem->size_tmp_tab_room != 0)
+			{
+				i = elem->tmp_tab_room[elem->size_tmp_tab_room - 1];
+				weight= weight + 1;
+				ft_add_weight(elem, matrice, weight, i)
+				elem->size_tmp_tab_room = elem->size_tmp_tab_room - 1;
+			}
+		}
+	}
+	ft_print_matrice_weight(matrice, elem);
+}
+
+/*
 **
 */
 /*
