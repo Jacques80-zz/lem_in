e42c25594a7a0f235d06e39c198710b78820fd70
Fawzi ALLOUCHE <fallouch@e2r8p6.42.fr>
2019-02-19 15:30:24 +0100

    optimisation de l'algo

--- sources/li_03_algo.c
+++ sources/li_03_algo.c
@@ -6,7 +6,7 @@
 /*   By: jdouniol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 05:31:20 by jdouniol          #+#    #+#             */
-/*   Updated: 2019/02/18 05:31:22 by jdouniol         ###   ########.fr       */
+/*   Updated: 2019/02/19 15:30:17 by fallouch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,13 +54,22 @@ void		delete_bad_link(t_all elem, int **matrice_flow)
 	}
 }
 
+t_path 		*return_bfs(t_files *file, t_files *tmp)
+{
+	t_path			*path;
+
+	free_file(file);
+	path = tmp->path;
+	free(tmp);
+	return (path);
+}
+
 t_path		*bfs(t_all elem, t_room ***matrice, int **matrice_flow, t_room *start, int bfs) // TODO trop de parametres et trop de lignes, et certianes lignes trop grande
 {
 	int				i;
 	int				j;
 	t_files			*file;
 	t_files			*tmp;
-	t_path 			*path;
 
 	file = NULL;
 	push_file(&file, start, NULL);
@@ -68,23 +77,16 @@ t_path		*bfs(t_all elem, t_room ***matrice, int **matrice_flow, t_room *start, i
 	{
 		tmp = pop_file(&file);
 		i = tmp->room->room_id;
-		j = 0;
+		j = -1;
 		if (tmp->room->status == END)
-		{
-			free_file(file);
-			path = tmp->path;
-			free(tmp);
-			return (path);
-		}
-		while (j < elem.number_rooms)
-		{
-			if (matrice[i][j] && matrice[i][j]->available != VISITED && matrice_flow[i][j] != 1 && matrice[i][j]->bfs != bfs)
+			return (return_bfs(file, tmp));
+		while (++j < elem.number_rooms)
+			if (matrice[i][j] && matrice[i][j]->available != VISITED
+				&& matrice_flow[i][j] != 1 && matrice[i][j]->bfs != bfs)
 			{
 				matrice[i][j]->bfs = bfs;
 				push_file(&file, matrice[i][j], path_cpy(tmp->path));
 			}
-			j++;
-		}
 		free_path(tmp->path);
 		free(tmp);
 	}
@@ -164,7 +166,7 @@ t_tab_path	*edmond_karp(t_all *elem, t_room ***matrice, int **matrice_flow, t_ro
 	}
 	tab = return_tab_path(*elem, matrice, matrice_flow, start);
 	ft_printf("tab size = %d", tab_size(tab));
-	tab = remove_bad_path(tab); // TODO leak quand pas de chemin dispo
+//	tab = remove_bad_path(tab); // TODO leak quand pas de chemin dispo
 	if (check_matrice_flow(*elem, matrice_flow))
 		ft_putendl("y a un flow qui reste");
 	ft_printf("tab size = %d", tab_size(tab));
